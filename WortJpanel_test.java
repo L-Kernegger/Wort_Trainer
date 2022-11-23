55d49633f92890a612452179deba39ac4a46b0eb
L.Kernegger <lkernegger@student.tgm.ac.at>
2022-11-24 00:09:48 +0100

    absolut horrender GUI Versuch

--- WortJpanel_test.java
+++ WortJpanel_test.java
@@ -2,30 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.net.*;
-public class WortJpanel extends JFrame{
+public class WortJpanel_test extends JFrame{
 
     public void panel() {
         JFrame frame = new JFrame(); //JFrame Creation
              
-        frame.setTitle("Add Image"); //Add the title to frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Terminate program on close button
-        frame.setBounds(100, 200, 350, 300); //Sets the position of the frame
-        Container c = frame.getContentPane(); //Gets the content layer
+        frame.setTitle("Add Image"); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setBounds(100, 200, 350, 300); 
+        Container c = frame.getContentPane(); 
         frame.setLayout(new BorderLayout());
-        JLabel label = new JLabel(); //JLabel Creation
-        label.setIcon(new ImageIcon("test.jpg")); //Sets the image to be displayed as an icon
-        Dimension size = label.getPreferredSize(); //Gets the size of the image
-        label.setBounds(100, 200, size.width, size.height); //Sets the location of the image
-        c.add(label); //Adds objects to the container
+        JLabel label = new JLabel(); 
+        label.setIcon(new ImageIcon("test.jpg")); 
+        Dimension size = label.getPreferredSize(); 
+        label.setBounds(100, 200, size.width, size.height); 
+        c.setLayout(getLayout());
+        c.add(label, BorderLayout.CENTER); //Adds objects to the container
         
-        Container c1 = new Container();
-        c1.setLayout(new BorderLayout());
-
         JLabel j1 = new JLabel("Was wird hier angezeigt?");
         c.add(j1, BorderLayout.NORTH);
         JTextField k = new JTextField();
         JLabel j2 = new JLabel();
-        frame.add(k,BorderLayout.SOUTH);
+        JPanel h = new JPanel();
+        j2.add(k);
+        ((JComponent) c).setBorder(BorderFactory.createDashedBorder(new Color(255, 0, 0)));
+        j2.add(new JLabel("Tetst"), BorderLayout.NORTH);
+        c.add(j2,BorderLayout.SOUTH);
 
         JButton t = new JButton("submit");
         t.setSize(10, 10);
